173499f082b684facc42803f3731410ec081f276
james hadfield <jh22@sanger.ac.uk>
2020-04-17 12:05:24 +1200

    Restore functionality of dropped files

--- auspice_client_customisation/handleDroppedFiles.js
+++ auspice_client_customisation/handleDroppedFiles.js
@@ -2,51 +2,37 @@ import { createStateFromQueryOrJSONs } from "@auspice/actions/recomputeReduxStat
 
 
 export const handleDroppedFiles = (dispatch, files) => {
-  const treeReader = new window.FileReader();
-  const metaReader = new window.FileReader();
 
-  const fileReadComplete = () => {
-    if (treeReader.result && metaReader.result) {
-      const json = {
-        tree: JSON.parse(treeReader.result),
-        meta: JSON.parse(metaReader.result),
-        _source: "dropped",
-        _treeName: undefined,
-        _url: "droppedFiles"
-      };
-
-      console.log("JSON IS:", json);
-
-      dispatch({
-        type: "CLEAN_START",
-        ...createStateFromQueryOrJSONs({json: json, query: {}})
-      });
-      dispatch({
-        type: "PAGE_CHANGE",
-        displayComponent: "main"
-      });
-
-    } else {
-      console.log("one file read...");
-    }
-  };
-  const onerror = (err) => {
-    console.warn("bugger!", err);
-  };
-
-
-  treeReader.onload = fileReadComplete;
-  metaReader.onload = fileReadComplete;
-  treeReader.onerror = onerror;
-  metaReader.onerror = onerror;
-
-  console.log("READING FILES!!!!")
-  for (const file of files) {
-    if (file.name.endsWith("_meta.json")) {
-      metaReader.readAsText(file);
-    } else if (file.name.endsWith("_tree.json")) {
-      treeReader.readAsText(file);
-    }
+  const v2DatasetReader = new window.FileReader();
+  v2DatasetReader.onloadstart = () => {
+    console.log("Reading JSON");
   }
+  v2DatasetReader.onload = (event) => {
+    const json = JSON.parse(event.target.result);
+    console.log("JSON", json);
+    /* The following is undocumented */
+    dispatch({
+      type: "CLEAN_START",
+      ...createStateFromQueryOrJSONs({json: json, query: {}})
+    });
+    dispatch({
+      type: "PAGE_CHANGE",
+      displayComponent: "main"
+    });
+  }
+  v2DatasetReader.onerror = handleError;
 
+  if (files.length > 1 || !files[0].name.endsWith(".json")) {
+    console.error("Can only process a single JSON currently");
+    return;
+  }
+  console.log(files, v2DatasetReader);
+  v2DatasetReader.readAsText(files[0]);
+
+};
+
+
+function handleError(err) {
+  console.error("Error reading JSON", err);
+  console.error("Please make a GitHub Issue so we can fix this!")
 };
